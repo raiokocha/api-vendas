20d1dec7e266a9ab3828aa4b5147c5dbe6c36306
raiokocha <30579803+raiokocha@users.noreply.github.com>
2021-08-10 15:56:00 -0300

    Profile + Email

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import productsRouter from '@modules/products/routes/products.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
+import passwordRouter from '@modules/users/routes/password.routes';
+import profileRouter from '@modules/users/routes/profile.routes';
 
 const routes = Router();
 
@@ -11,5 +13,7 @@ routes.get('/', (request, response) => {
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
+routes.use('/profile', profileRouter);
 
 export default routes;
